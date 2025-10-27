a0dc3a4f34734df5ded19274c5e30b874570ca7d
Eric Bai <3200769991@qq.com>
2025-10-27 20:52:07 +0800

    chore: 优化实现：link-presets.ts

---
+++ src/constants/link-presets.ts
@@ -0,0 +1,24 @@
+import { LinkPreset, type NavBarLink } from "@/types/config";
+
+export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
+	[LinkPreset.Home]: {
+		name: "首页",
+		url: "/",
+	},
+	[LinkPreset.Archive]: {
+		name: "归档",
+		url: "/archive/",
+	},
+	[LinkPreset.Friends]: {
+		name: "友链",
+		url: "/friends/",
+	},
+	[LinkPreset.Apps]: {
+		name: "应用",
+		url: "/apps/",
+	},
+	[LinkPreset.Donate]: {
+		name: "赞助",
+		url: "/donate/",
+	},
+};
