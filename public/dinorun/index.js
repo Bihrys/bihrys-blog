2c1ad420f1c014bb5cb006609a4fdf9e86f4c5a6
Eric Bai <3200769991@qq.com>
2026-06-30 23:58:11 +0800

    feat: 补充工具函数（dinorun）

--- public/dinorun/index.js
+++ public/dinorun/index.js
@@ -85,3 +85,90 @@
      * @const
      */
     var FPS = 60;
+
+    /** @const */
+    var IS_HIDPI = window.devicePixelRatio > 1;
+
+    /** @const */
+    var IS_IOS = /iPad|iPhone|iPod/.test(window.navigator.platform);
+
+    /** @const */
+    var IS_MOBILE = /Android/.test(window.navigator.userAgent) || IS_IOS;
+
+    /** @const */
+    var IS_TOUCH_ENABLED = 'ontouchstart' in window;
+
+    /**
+     * Default game configuration.
+     * @enum {number}
+     */
+    Runner.config = {
+        ACCELERATION: 0.001,
+        BG_CLOUD_SPEED: 0.2,
+        BOTTOM_PAD: 10,
+        CLEAR_TIME: 3000,
+        CLOUD_FREQUENCY: 0.5,
+        GAMEOVER_CLEAR_TIME: 750,
+        GAP_COEFFICIENT: 0.6,
+        GRAVITY: 0.6,
+        INITIAL_JUMP_VELOCITY: 12,
+        INVERT_FADE_DURATION: 12000,
+        INVERT_DISTANCE: 700,
+        MAX_BLINK_COUNT: 3,
+        MAX_CLOUDS: 6,
+        MAX_OBSTACLE_LENGTH: 3,
+        MAX_OBSTACLE_DUPLICATION: 2,
+        MAX_SPEED: 13,
+        MIN_JUMP_HEIGHT: 35,
+        MOBILE_SPEED_COEFFICIENT: 1.2,
+        RESOURCE_TEMPLATE_ID: 'audio-resources',
+        SPEED: 6,
+        SPEED_DROP_COEFFICIENT: 3
+    };
+
+
+    /**
+     * Default dimensions.
+     * @enum {string}
+     */
+    Runner.defaultDimensions = {
+        WIDTH: DEFAULT_WIDTH,
+        HEIGHT: 150
+    };
+
+
+    /**
+     * CSS class names.
+     * @enum {string}
+     */
+    Runner.classes = {
+        CANVAS: 'runner-canvas',
+        CONTAINER: 'runner-container',
+        CRASHED: 'crashed',
+        ICON: 'icon-offline',
+        INVERTED: 'inverted',
+        SNACKBAR: 'snackbar',
+        SNACKBAR_SHOW: 'snackbar-show',
+        TOUCH_CONTROLLER: 'controller'
+    };
+
+
+    /**
+     * Sprite definition layout of the spritesheet.
+     * @enum {Object}
+     */
+    Runner.spriteDefinition = {
+        LDPI: {
+            CACTUS_LARGE: { x: 332, y: 2 },
+            CACTUS_SMALL: { x: 228, y: 2 },
+            CLOUD: { x: 86, y: 2 },
+            HORIZON: { x: 2, y: 54 },
+            MOON: { x: 484, y: 2 },
+            PTERODACTYL: { x: 134, y: 2 },
+            RESTART: { x: 2, y: 2 },
+            TEXT_SPRITE: { x: 655, y: 2 },
+            TREX: { x: 848, y: 2 },
+            STAR: { x: 645, y: 2 }
+        },
+        HDPI: {
+            CACTUS_LARGE: { x: 652, y: 2 },
