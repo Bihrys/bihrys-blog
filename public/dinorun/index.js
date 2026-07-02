873961796b67397686d50caed6816f66e1ad01fa
Eric Bai <3200769991@qq.com>
2026-07-02 23:20:17 +0800

    feat: 添加功能实现（dinorun）

--- public/dinorun/index.js
+++ public/dinorun/index.js
@@ -433,3 +433,90 @@
                 this.distanceMeter.calcXPos(this.dimensions.WIDTH);
                 this.clearCanvas();
                 this.horizon.update(0, 0, true);
+                this.tRex.update(0);
+
+                // Outer container and distance meter.
+                if (this.playing || this.crashed || this.paused) {
+                    this.containerEl.style.width = this.dimensions.WIDTH + 'px';
+                    this.containerEl.style.height = this.dimensions.HEIGHT + 'px';
+                    this.distanceMeter.update(0, Math.ceil(this.distanceRan));
+                    this.stop();
+                } else {
+                    this.tRex.draw(0, 0);
+                }
+
+                // Game over panel.
+                if (this.crashed && this.gameOverPanel) {
+                    this.gameOverPanel.updateDimensions(this.dimensions.WIDTH);
+                    this.gameOverPanel.draw();
+                }
+            }
+        },
+
+        /**
+         * Play the game intro.
+         * Canvas container width expands out to the full width.
+         */
+        playIntro: function () {
+            if (!this.activated && !this.crashed) {
+                this.playingIntro = true;
+                this.tRex.playingIntro = true;
+
+                // CSS animation definition.
+                var keyframes = '@-webkit-keyframes intro { ' +
+                    'from { width:' + Trex.config.WIDTH + 'px }' +
+                    'to { width: ' + this.dimensions.WIDTH + 'px }' +
+                    '}';
+                
+                // create a style sheet to put the keyframe rule in 
+                // and then place the style sheet in the html head    
+                var sheet = document.createElement('style');
+                sheet.innerHTML = keyframes;
+                document.head.appendChild(sheet);
+
+                this.containerEl.addEventListener(Runner.events.ANIM_END,
+                    this.startGame.bind(this));
+
+                this.containerEl.style.webkitAnimation = 'intro .4s ease-out 1 both';
+                this.containerEl.style.width = this.dimensions.WIDTH + 'px';
+
+                // if (this.touchController) {
+                //     this.outerContainerEl.appendChild(this.touchController);
+                // }
+                this.playing = true;
+                this.activated = true;
+            } else if (this.crashed) {
+                this.restart();
+            }
+        },
+
+
+        /**
+         * Update the game status to started.
+         */
+        startGame: function () {
+            this.runningTime = 0;
+            this.playingIntro = false;
+            this.tRex.playingIntro = false;
+            this.containerEl.style.webkitAnimation = '';
+            this.playCount++;
+
+            // Handle tabbing off the page. Pause the current game.
+            document.addEventListener(Runner.events.VISIBILITY,
+                this.onVisibilityChange.bind(this));
+
+            window.addEventListener(Runner.events.BLUR,
+                this.onVisibilityChange.bind(this));
+
+            window.addEventListener(Runner.events.FOCUS,
+                this.onVisibilityChange.bind(this));
+        },
+
+        clearCanvas: function () {
+            this.canvasCtx.clearRect(0, 0, this.dimensions.WIDTH,
+                this.dimensions.HEIGHT);
+        },
+
+        /**
+         * Update the game frame and schedules the next one.
+         */
