f021096513ef7d9b4dd3f2e31729a290c5946fd9
Eric Bai <3200769991@qq.com>
2026-07-15 20:52:03 +0800

    fix: 修正拼写：index.js

--- public/dinorun/index.js
+++ public/dinorun/index.js
@@ -1651,3 +1651,90 @@
             }
 
             // Game intro animation, T-rex moves in from the left.
+            if (this.playingIntro && this.xPos < this.config.START_X_POS) {
+                this.xPos += Math.round((this.config.START_X_POS /
+                    this.config.INTRO_DURATION) * deltaTime);
+            }
+
+            if (this.status == Trex.status.WAITING) {
+                this.blink(getTimeStamp());
+            } else {
+                this.draw(this.currentAnimFrames[this.currentFrame], 0);
+            }
+
+            // Update the frame position.
+            if (this.timer >= this.msPerFrame) {
+                this.currentFrame = this.currentFrame ==
+                    this.currentAnimFrames.length - 1 ? 0 : this.currentFrame + 1;
+                this.timer = 0;
+            }
+
+            // Speed drop becomes duck if the down key is still being pressed.
+            if (this.speedDrop && this.yPos == this.groundYPos) {
+                this.speedDrop = false;
+                this.setDuck(true);
+            }
+        },
+
+        /**
+         * Draw the t-rex to a particular position.
+         * @param {number} x
+         * @param {number} y
+         */
+        draw: function (x, y) {
+            var sourceX = x;
+            var sourceY = y;
+            var sourceWidth = this.ducking && this.status != Trex.status.CRASHED ?
+                this.config.WIDTH_DUCK : this.config.WIDTH;
+            var sourceHeight = this.config.HEIGHT;
+
+            if (IS_HIDPI) {
+                sourceX *= 2;
+                sourceY *= 2;
+                sourceWidth *= 2;
+                sourceHeight *= 2;
+            }
+
+            // Adjustments for sprite sheet position.
+            sourceX += this.spritePos.x;
+            sourceY += this.spritePos.y;
+
+            // Ducking.
+            if (this.ducking && this.status != Trex.status.CRASHED) {
+                this.canvasCtx.drawImage(Runner.imageSprite, sourceX, sourceY,
+                    sourceWidth, sourceHeight,
+                    this.xPos, this.yPos,
+                    this.config.WIDTH_DUCK, this.config.HEIGHT);
+            } else {
+                // Crashed whilst ducking. Trex is standing up so needs adjustment.
+                if (this.ducking && this.status == Trex.status.CRASHED) {
+                    this.xPos++;
+                }
+                // Standing / running
+                this.canvasCtx.drawImage(Runner.imageSprite, sourceX, sourceY,
+                    sourceWidth, sourceHeight,
+                    this.xPos, this.yPos,
+                    this.config.WIDTH, this.config.HEIGHT);
+            }
+        },
+
+        /**
+         * Sets a random time for the blink to happen.
+         */
+        setBlinkDelay: function () {
+            this.blinkDelay = Math.ceil(Math.random() * Trex.BLINK_TIMING);
+        },
+
+        /**
+         * Make t-rex blink at random intervals.
+         * @param {number} time Current time in milliseconds.
+         */
+        blink: function (time) {
+            var deltaTime = time - this.animStartTime;
+
+            if (deltaTime >= this.blinkDelay) {
+                this.draw(this.currentAnimFrames[this.currentFrame], 0);
+
+                if (this.currentFrame == 1) {
+                    // Set new random delay to blink.
+                    this.setBlinkDelay();
