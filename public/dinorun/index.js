7cfeeab56b43fe0b642488be83b5ec2ae1406db9
Eric Bai <3200769991@qq.com>
2026-07-19 21:35:42 +0800

    refactor: 拆分函数（dinorun）

--- public/dinorun/index.js
+++ public/dinorun/index.js
@@ -1825,3 +1825,90 @@
             } else if (this.status == Trex.status.DUCKING) {
                 this.update(0, Trex.status.RUNNING);
                 this.ducking = false;
+            }
+        },
+
+        /**
+         * Reset the t-rex to running at start of game.
+         */
+        reset: function () {
+            this.yPos = this.groundYPos;
+            this.jumpVelocity = 0;
+            this.jumping = false;
+            this.ducking = false;
+            this.update(0, Trex.status.RUNNING);
+            this.midair = false;
+            this.speedDrop = false;
+            this.jumpCount = 0;
+        }
+    };
+
+
+    //******************************************************************************
+
+    /**
+     * Handles displaying the distance meter.
+     * @param {!HTMLCanvasElement} canvas
+     * @param {Object} spritePos Image position in sprite.
+     * @param {number} canvasWidth
+     * @constructor
+     */
+    function DistanceMeter(canvas, spritePos, canvasWidth) {
+        this.canvas = canvas;
+        this.canvasCtx = canvas.getContext('2d');
+        this.image = Runner.imageSprite;
+        this.spritePos = spritePos;
+        this.x = 0;
+        this.y = 5;
+
+        this.currentDistance = 0;
+        this.maxScore = 0;
+        this.highScore = 0;
+        this.container = null;
+
+        this.digits = [];
+        this.acheivement = false;
+        this.defaultString = '';
+        this.flashTimer = 0;
+        this.flashIterations = 0;
+        this.invertTrigger = false;
+
+        this.config = DistanceMeter.config;
+        this.maxScoreUnits = this.config.MAX_DISTANCE_UNITS;
+        this.init(canvasWidth);
+    };
+
+
+    /**
+     * @enum {number}
+     */
+    DistanceMeter.dimensions = {
+        WIDTH: 10,
+        HEIGHT: 13,
+        DEST_WIDTH: 11
+    };
+
+
+    /**
+     * Y positioning of the digits in the sprite sheet.
+     * X position is always 0.
+     * @type {Array<number>}
+     */
+    DistanceMeter.yPos = [0, 13, 27, 40, 53, 67, 80, 93, 107, 120];
+
+
+    /**
+     * Distance meter config.
+     * @enum {number}
+     */
+    DistanceMeter.config = {
+        // Number of digits.
+        MAX_DISTANCE_UNITS: 5,
+
+        // Distance that causes achievement animation.
+        ACHIEVEMENT_DISTANCE: 100,
+
+        // Used for conversion from pixel distance to a scaled unit.
+        COEFFICIENT: 0.025,
+
+        // Flash duration in milliseconds.
