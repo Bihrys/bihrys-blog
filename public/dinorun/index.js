1bb486449e76fe03319f15067b3d619da2bb509d
Eric Bai <3200769991@qq.com>
2026-07-21 20:59:39 +0800

    fix: 修正拼写（dinorun）

--- public/dinorun/index.js
+++ public/dinorun/index.js
@@ -1999,3 +1999,90 @@
          * @param {number} distance Pixel distance ran.
          * @return {number} The 'real' distance ran.
          */
+        getActualDistance: function (distance) {
+            return distance ? Math.round(distance * this.config.COEFFICIENT) : 0;
+        },
+
+        /**
+         * Update the distance meter.
+         * @param {number} distance
+         * @param {number} deltaTime
+         * @return {boolean} Whether the acheivement sound fx should be played.
+         */
+        update: function (deltaTime, distance) {
+            var paint = true;
+            var playSound = false;
+
+            if (!this.acheivement) {
+                distance = this.getActualDistance(distance);
+                // Score has gone beyond the initial digit count.
+                if (distance > this.maxScore && this.maxScoreUnits ==
+                    this.config.MAX_DISTANCE_UNITS) {
+                    this.maxScoreUnits++;
+                    this.maxScore = parseInt(this.maxScore + '9');
+                } else {
+                    this.distance = 0;
+                }
+
+                if (distance > 0) {
+                    // Acheivement unlocked
+                    if (distance % this.config.ACHIEVEMENT_DISTANCE == 0) {
+                        // Flash score and play sound.
+                        this.acheivement = true;
+                        this.flashTimer = 0;
+                        playSound = true;
+                    }
+
+                    // Create a string representation of the distance with leading 0.
+                    var distanceStr = (this.defaultString +
+                        distance).substr(-this.maxScoreUnits);
+                    this.digits = distanceStr.split('');
+                } else {
+                    this.digits = this.defaultString.split('');
+                }
+            } else {
+                // Control flashing of the score on reaching acheivement.
+                if (this.flashIterations <= this.config.FLASH_ITERATIONS) {
+                    this.flashTimer += deltaTime;
+
+                    if (this.flashTimer < this.config.FLASH_DURATION) {
+                        paint = false;
+                    } else if (this.flashTimer >
+                        this.config.FLASH_DURATION * 2) {
+                        this.flashTimer = 0;
+                        this.flashIterations++;
+                    }
+                } else {
+                    this.acheivement = false;
+                    this.flashIterations = 0;
+                    this.flashTimer = 0;
+                }
+            }
+
+            // Draw the digits if not flashing.
+            if (paint) {
+                for (var i = this.digits.length - 1; i >= 0; i--) {
+                    this.draw(i, parseInt(this.digits[i]));
+                }
+            }
+
+            this.drawHighScore();
+            return playSound;
+        },
+
+        /**
+         * Draw the high score.
+         */
+        drawHighScore: function () {
+            this.canvasCtx.save();
+            this.canvasCtx.globalAlpha = .8;
+            for (var i = this.highScore.length - 1; i >= 0; i--) {
+                this.draw(i, parseInt(this.highScore[i], 10), true);
+            }
+            this.canvasCtx.restore();
+        },
+
+        /**
+         * Set the highscore as a array string.
+         * Position of char in the sprite: H - 10, I - 11.
+         * @param {number} distance Distance ran in pixels.
