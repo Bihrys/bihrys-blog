1e7ec0cbe48a284858d30e481abf153cec606943
Eric Bai <3200769991@qq.com>
2026-07-22 22:01:03 +0800

    chore: 优化实现（dinorun/index.js）

--- public/dinorun/index.js
+++ public/dinorun/index.js
@@ -2086,3 +2086,90 @@
          * Set the highscore as a array string.
          * Position of char in the sprite: H - 10, I - 11.
          * @param {number} distance Distance ran in pixels.
+         */
+        setHighScore: function (distance) {
+            distance = this.getActualDistance(distance);
+            var highScoreStr = (this.defaultString +
+                distance).substr(-this.maxScoreUnits);
+
+            this.highScore = ['10', '11', ''].concat(highScoreStr.split(''));
+        },
+
+        /**
+         * Reset the distance meter back to '00000'.
+         */
+        reset: function () {
+            this.update(0);
+            this.acheivement = false;
+        }
+    };
+
+
+    //******************************************************************************
+
+    /**
+     * Cloud background item.
+     * Similar to an obstacle object but without collision boxes.
+     * @param {HTMLCanvasElement} canvas Canvas element.
+     * @param {Object} spritePos Position of image in sprite.
+     * @param {number} containerWidth
+     */
+    function Cloud(canvas, spritePos, containerWidth) {
+        this.canvas = canvas;
+        this.canvasCtx = this.canvas.getContext('2d');
+        this.spritePos = spritePos;
+        this.containerWidth = containerWidth;
+        this.xPos = containerWidth;
+        this.yPos = 0;
+        this.remove = false;
+        this.cloudGap = getRandomNum(Cloud.config.MIN_CLOUD_GAP,
+            Cloud.config.MAX_CLOUD_GAP);
+
+        this.init();
+    };
+
+
+    /**
+     * Cloud object config.
+     * @enum {number}
+     */
+    Cloud.config = {
+        HEIGHT: 14,
+        MAX_CLOUD_GAP: 400,
+        MAX_SKY_LEVEL: 30,
+        MIN_CLOUD_GAP: 100,
+        MIN_SKY_LEVEL: 71,
+        WIDTH: 46
+    };
+
+
+    Cloud.prototype = {
+        /**
+         * Initialise the cloud. Sets the Cloud height.
+         */
+        init: function () {
+            this.yPos = getRandomNum(Cloud.config.MAX_SKY_LEVEL,
+                Cloud.config.MIN_SKY_LEVEL);
+            this.draw();
+        },
+
+        /**
+         * Draw the cloud.
+         */
+        draw: function () {
+            this.canvasCtx.save();
+            var sourceWidth = Cloud.config.WIDTH;
+            var sourceHeight = Cloud.config.HEIGHT;
+
+            if (IS_HIDPI) {
+                sourceWidth = sourceWidth * 2;
+                sourceHeight = sourceHeight * 2;
+            }
+
+            this.canvasCtx.drawImage(Runner.imageSprite, this.spritePos.x,
+                this.spritePos.y,
+                sourceWidth, sourceHeight,
+                this.xPos, this.yPos,
+                Cloud.config.WIDTH, Cloud.config.HEIGHT);
+
+            this.canvasCtx.restore();
