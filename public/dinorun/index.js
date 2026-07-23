f32b37dc3deda6ca20f6c28ac71514ecbc1ecfbb
Eric Bai <3200769991@qq.com>
2026-07-23 23:45:05 +0800

    fix: 修复边界情况

--- public/dinorun/index.js
+++ public/dinorun/index.js
@@ -2347,3 +2347,90 @@
                 }
             }
         },
+
+        reset: function () {
+            this.currentPhase = 0;
+            this.opacity = 0;
+            this.update(false);
+        }
+
+    };
+
+
+    //******************************************************************************
+
+    /**
+     * Horizon Line.
+     * Consists of two connecting lines. Randomly assigns a flat / bumpy horizon.
+     * @param {HTMLCanvasElement} canvas
+     * @param {Object} spritePos Horizon position in sprite.
+     * @constructor
+     */
+    function HorizonLine(canvas, spritePos) {
+        this.spritePos = spritePos;
+        this.canvas = canvas;
+        this.canvasCtx = canvas.getContext('2d');
+        this.sourceDimensions = {};
+        this.dimensions = HorizonLine.dimensions;
+        this.sourceXPos = [this.spritePos.x, this.spritePos.x +
+            this.dimensions.WIDTH];
+        this.xPos = [];
+        this.yPos = 0;
+        this.bumpThreshold = 0.5;
+
+        this.setSourceDimensions();
+        this.draw();
+    };
+
+
+    /**
+     * Horizon line dimensions.
+     * @enum {number}
+     */
+    HorizonLine.dimensions = {
+        WIDTH: 600,
+        HEIGHT: 12,
+        YPOS: 127
+    };
+
+
+    HorizonLine.prototype = {
+        /**
+         * Set the source dimensions of the horizon line.
+         */
+        setSourceDimensions: function () {
+
+            for (var dimension in HorizonLine.dimensions) {
+                if (IS_HIDPI) {
+                    if (dimension != 'YPOS') {
+                        this.sourceDimensions[dimension] =
+                            HorizonLine.dimensions[dimension] * 2;
+                    }
+                } else {
+                    this.sourceDimensions[dimension] =
+                        HorizonLine.dimensions[dimension];
+                }
+                this.dimensions[dimension] = HorizonLine.dimensions[dimension];
+            }
+
+            this.xPos = [0, HorizonLine.dimensions.WIDTH];
+            this.yPos = HorizonLine.dimensions.YPOS;
+        },
+
+        /**
+         * Return the crop x position of a type.
+         */
+        getRandomType: function () {
+            return Math.random() > this.bumpThreshold ? this.dimensions.WIDTH : 0;
+        },
+
+        /**
+         * Draw the horizon line.
+         */
+        draw: function () {
+            this.canvasCtx.drawImage(Runner.imageSprite, this.sourceXPos[0],
+                this.spritePos.y,
+                this.sourceDimensions.WIDTH, this.sourceDimensions.HEIGHT,
+                this.xPos[0], this.yPos,
+                this.dimensions.WIDTH, this.dimensions.HEIGHT);
+
